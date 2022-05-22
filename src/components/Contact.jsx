0c852feaa465a61db3ce26976cd5dfb5d6667130
hiroki <hiroki12malu@gmail.com>
2022-05-22 23:06:18 +0900

    コンタクト、スキルコンポーネント追加

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+export const Contact = () => {
+  return (
+    <div id="contact" className="contact">
+      <div className="container">
+        <div className="contact-contents">
+          <h2>Contact to Me!</h2>
+          <p className="description"></p>
+          <a href="https://twitter.com/1205malumalu" className="btn">Twitter</a>
+        </div>
+      </div>
+    </div>
+  );
+};
